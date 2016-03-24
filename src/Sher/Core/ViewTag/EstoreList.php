7481640fa69cbf45c53c10885a47ec300c79402d
purpen <purpen.w@gmail.com>
2016-03-25 00:24:30 +0800

    add store.

--- src/Sher/Core/ViewTag/EstoreList.php
+++ src/Sher/Core/ViewTag/EstoreList.php
@@ -1,53 +1,54 @@
 <?php
 /**
- * 省份列表标签
- * @author tianshuai
+ * 店铺列表标签
+ * @author purpen
  */
-class Sher_Core_ViewTag_ProvinceList extends Doggy_Dt_Tag {
-    protected $argstring;
-	
+class Sher_Core_ViewTag_EstoreList extends Doggy_Dt_Tag {
+    
+	protected $argstring;
     public function __construct($argstring, $parser, $pos = 0) {
         $this->argstring = $argstring;
     }
-
+	
     /**
      * 列表的条件保持与索引顺序一致(non-PHPdoc)
      * @see Doggy/Dt/Doggy_Dt_Node#render()
      */
     public function render($context, $stream) {
-        $page = 1;
+		$page = 1;
         $size = 10;
-		
-		$pid = 0;
-		
+        
+        $city_id = 0;
+        
+		$sort = 'latest';
+        
         $var = 'list';
         $include_pager = 0;
         $pager_var = 'pager';
-		$sort_field = 'asc';
-
-        extract($this->resolve_args($context, $this->argstring, EXTR_IF_EXISTS));
-
+		
+        extract($this->resolve_args($context,$this->argstring,EXTR_IF_EXISTS));
+		
         $page = (int) $page;
         $page || $page = 1;
         $size = (int)$size;
 		
         $query = array();
-		
-		if ($pid) {
-			$query['pid'] = (int)$pid;
+        
+		if ($city_id) {
+			$query['city_id'] = (int)$city_id;
 		}
 		
-        $service = Sher_Core_Service_Province::instance();
+        $service = Sher_Core_Service_Estore::instance();
+        
         $options['page'] = $page;
         $options['size'] = $size;
-		$options['sort_field'] = $sort_field;
-		
-        $result = $service->get_province_list($query,$options);
-		        
-        $context->set($var, $result);
+		$options['sort_field'] = $sort;
+        
+        $result = $service->get_store_list($query, $options);
+        $context->set($var,$result);
+        
         if ($include_pager) {
             $context->set($pager_var,$result['pager']);
         }
-        
     }
 }
